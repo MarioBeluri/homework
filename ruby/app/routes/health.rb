0143bec10b3f08254629a79a52371ed9e6ccf53e
MarioBeluri <mariobeluri@gmail.com>
2023-05-15 17:30:44 +0200

    Made some chnages to the routes/ articles.rb

--- ruby/app/routes/health.rb
+++ ruby/app/routes/health.rb
@@ -1,8 +1,9 @@
 class HealthRoutes < Sinatra::Base
     
   get('/') do
-    if request.env['AUTHED'] == true
-     'App working OK'
+    if request.env['HTTP_AUTHED'] == true
+    status 200
+    'App working OK'
     else
       status 403
     end
